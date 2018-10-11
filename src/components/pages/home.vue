976c056e0f89746207d93f261b5fbc67e3030ca9
humengchuan123 <531537052@qq.com>
2018-10-11 23:26:20 +0800

    finish header

---
+++ src/components/pages/home.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<home-header></home-header>
+	</div>
+</template>
+
+<script>
+import homeHeader from '../pages/homeheader'
+export default {
+
+  name: 'home',
+
+  data () {
+    return {
+
+    }
+  },
+  components: {
+  	homeHeader:homeHeader
+  }
+}
+</script>
+<style></style>
