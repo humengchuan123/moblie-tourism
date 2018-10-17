91484912e24893ac4e5a2c93346c3604199f4a0f
humengchuan123 <531537052@qq.com>
2018-10-17 16:30:42 +0800

    finish home page

--- src/components/pages/recommend.vue
+++ src/components/pages/recommend.vue
@@ -1,24 +1,70 @@
 <template>
-  <div class="wrapper">
-    hello I'm Recommend
+  <div>
+      <div class="title">热销推荐</div>
+      <ul>
+        <li class="item border-bottom" v-for="item of list" :key="item.id">
+          <img class="item-img" :src="item.imgUrl" alt="">
+          <div class="item-info">
+             <p class="item-title">{{item.title}}</p>
+             <p class="item-desc">{{item.desc}}</p>
+             <button class="item-btn">查看详情</button>
+          </div>
+        </li>
+      </ul>
   </div>
 </template>
 
 <script>
 export default {
-  components:{},
-  props:{},
+  props:{
+    list: Array
+  },
   data(){
     return {
     }
-  },
-  watch:{},
-  computed:{},
-  methods:{},
-  created(){},
-  mounted(){}
+  }
 }
 </script>
 <style lang="scss" scoped>
-.wrapper{}
+@import "~@/assets/styles/mixins";
+  .title{
+    margin-top: .2rem;
+    line-height: .8rem;
+    background: #eee;
+    text-indent: .2rem;
+  }
+  .item{
+    overflow: hidden;
+    display: flex;
+    height: 1.9rem;
+    .item-img{
+      width: 1.7rem;
+      height: 1.7rem;
+      padding: .1rem;
+    }
+    .item-info{
+      flex: 1;
+      padding: .1rem;
+      min-width: 0;
+      .item-title{
+        line-height: .54rem;
+        font-size: .32rem;
+        @include ellipsis;
+      }
+      .item-desc{
+        line-height: .4rem;
+        color: #cccccc;
+        @include ellipsis;
+      }
+      .item-btn{
+        line-height: .44rem;
+        margin-top: .16rem;
+        background: #ff9300;
+        padding: 0 .2rem;
+        border-radius: .06rem;
+        color: #fff;
+      }
+    }
+  }
+
 </style>
--- src/components/pages/wenkend.vue
+++ src/components/pages/wenkend.vue
@@ -1,24 +1,59 @@
 <template>
-  <div class="wrapper">
-    hello I'm wenkend
+  <div>
+      <div class="title">周末去哪儿</div>
+      <ul>
+        <li class="item border-bottom" v-for="item of list" :key="item.id">
+          <div class="item-img-wrap">
+            <img class="item-img" :src="item.imgUrl" alt="">
+          </div>
+          <div class="item-info">
+             <p class="item-title">{{item.title}}</p>
+             <p class="item-desc">{{item.desc}}</p>
+          </div>
+        </li>
+      </ul>
   </div>
 </template>
 
 <script>
 export default {
-  components:{},
-  props:{},
+  props:{
+    list: Array
+  },
   data(){
     return {
     }
-  },
-  watch:{},
-  computed:{},
-  methods:{},
-  created(){},
-  mounted(){}
+  }
 }
 </script>
 <style lang="scss" scoped>
-.wrapper{}
+@import "~@/assets/styles/mixins";
+  .title{
+    margin-top: .2rem;
+    line-height: .8rem;
+    background: #eee;
+    text-indent: .2rem;
+  }
+  .item-img-wrap{
+    overflow: hidden;
+    height: 0;
+    padding-bottom: 37.09%;
+    .item-img{
+      width: 100%;
+    }
+  }
+    .item-info{
+      padding: .1rem;
+      min-width: 0;
+      .item-title{
+        line-height: .54rem;
+        font-size: .32rem;
+        @include ellipsis;
+      }
+      .item-desc{
+        line-height: .4rem;
+        color: #cccccc;
+        @include ellipsis;
+      }
+    }
 </style>
